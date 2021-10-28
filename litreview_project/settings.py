467e6bf9c61424674aed983699889b1f2740fbf1
Didier K. Nzimbi <82230373+dbahsa@users.noreply.github.com>
2021-10-28 10:59:33 +0200

    updated html registration files

--- litreview_project/settings.py
+++ litreview_project/settings.py
@@ -133,3 +133,13 @@ LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+DEFAULT_FROM_EMAIL = 'dbahsa@gmail.com'
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey'
+EMAIL_HOST_PASSWORD = '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'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
