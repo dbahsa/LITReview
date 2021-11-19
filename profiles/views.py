27a81ddad2e7943707a18457e86fd4ebfcf7b8f4
Didier K. Nzimbi <82230373+dbahsa@users.noreply.github.com>
2021-11-19 16:20:06 +0100

    added and created users profiles from the backoffice

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Profile
 
-# Create your views here.
+
+class ProfileListView(ListView):
+    model = Profile
+
